2e29b6f1c9facf810ee1901d12991fb0639365c9
Conor Scheidt <conorrs@protonmail.com>
2023-01-13 18:45:27 -0600

    Change logging slightly and use consts as config

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 	"sync"
 	"time"
 
+	u "net/url"
+
 	"context"
 	"encoding/json"
 
-	"github.com/joho/godotenv"
 	"github.com/playwright-community/playwright-go"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -17,18 +18,41 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"strconv"
 )
 
-// ! DEFAULTS
+// ! Config
 const (
-	DEFAULT_MONGO_URI       string = "mongodb://127.0.0.1:27017"
-	DEFAULT_DB_NAME         string = "ephemerals"
-	DEFAULT_COLLECTION_NAME string = "sites"
-	DEFAULT_MAX_TABS     int    = 32
+	MONGO_URI       string = "mongodb://127.0.0.1:27017"
+	DB_NAME         string = "ephemerals"
+	COLLECTION_NAME string = "sites"
+
+	MAX_TABS     int    = 32
+	MAX_BROWSERS int    = 8
+	BROWSER_PATH string = "/home/cone/.cache/ms-playwright/chromium-1041/chrome-linux/chrome"
+
+	SITES_FILE string = "test.json"
+
+	// How longto wait in addition to "networkidle"
+	LOAD_WAIT time.Duration = time.Duration(15) * time.Second
 )
 
 type Input map[string][]string
+
+func (in Input) Chunks(size int) []Input {
+	res := make([]Input, len(in)/size)
+	chunk := make(Input, size)
+	i := 0
+	for k, v := range in {
+		chunk[k] = v
+		i++
+		if i == size || i == len(in) {
+			i = 0
+			res = append(res, chunk)
+			chunk = make(Input, size)
+		}
+	}
+	return res
+}
 
 // Represents a single top-level site in the database
 type Site struct {
@@ -39,7 +63,6 @@ type Site struct {
 	// Collection of trials for this URL
 	Trials []Trial `bson:"trials"`
 }
-
 type Trial struct {
 	// Browser version string
 	BrowserVersion string `bson:"browser_version"`
@@ -53,9 +76,10 @@ type Trial struct {
 
 type Request struct {
 	// URL of the request
-	Url string `bson:"url"`
+	Url u.URL `bson:"url"`
 	// Type of request
 	ResourceType string `bson:"resource_type"`
+	// TTL          uint32 `bson:"ttl"`
 }
 
 func chunks[T any](items []T, chunkSize int) (chunks [][]T) {
@@ -66,70 +90,81 @@ func chunks[T any](items []T, chunkSize int) (chunks [][]T) {
 }
 
 // Gets data for a given category and inserts it into the database
-func getCategory(urls []string, category string, bCtx playwright.BrowserContext, collection *mongo.Collection, wg *sync.WaitGroup, tabsPerCtx int) {
+func getCategory(urls *[]string, category string, bCtx *playwright.BrowserContext, collection *mongo.Collection, wg *sync.WaitGroup) {
 	defer wg.Done()
 	var wg2 sync.WaitGroup
 
-	urlBuffer := chunks(urls, tabsPerCtx)
+	urlBuffer := chunks(*urls, MAX_TABS)
 
 	for i, urlGroup := range urlBuffer {
-		LOG.Infof("Starting tab group %d", i)
+		LOG.Debugf("Starting tab group %d", i)
 		for _, url := range urlGroup {
-			page, e := bCtx.NewPage()
+			page, e := (*bCtx).NewPage()
 			if e != nil {
-				LOG.Panic("Failed to initialize new page for %s(%s): %v", url, category, e)
+				LOG.Panic("%s: Failed to create new page: %v", url, e)
 			}
 			wg2.Add(1)
-			go getSite(url, category, page, collection, &wg2)
+			go getSite(url, category, &page, collection, &wg2)
 		}
 		wg2.Wait()
 	}
 }
 
 // Gets data for a given site and inserts it into the database
-func getSite(url string, category string, page playwright.Page, collection *mongo.Collection, wg *sync.WaitGroup) {
+func getSite(url string, category string, page *playwright.Page, collection *mongo.Collection, wg *sync.WaitGroup) {
 	defer wg.Done()
-	defer page.Close()
+	defer (*page).Close()
 
-	LOG.Infof("Getting data for %s", url)
+	LOG.Debugf("%s: starting data collection", url)
 
 	var reqs []Request
 
-	page.On("request", func(req playwright.Request) {
-		// LOG.Debugf("Request from %s to %s", url, req.URL())
-		reqs = append(reqs, Request{Url: req.URL(), ResourceType: req.ResourceType()})
+	(*page).On("request", func(req playwright.Request) {
+		LOG.Debugf("%s -> %s", url, req.URL())
+		parsedUrl, e := u.ParseRequestURI(req.URL())
+		if e != nil {
+			LOG.Errorf("%s: failed to parse request to `%s`: %v", url, req.URL(), e)
+		} else {
+			//! Get TTL Debug here
+			reqs = append(reqs, Request{Url: *parsedUrl, ResourceType: req.ResourceType()})
+		}
 	})
 
+	// Load page
 	start := time.Now()
-	_, e := page.Goto(url)
+	_, e := (*page).Goto(url)
 
 	if e != nil {
-		LOG.Warnf("Failed to fully load %s, still inserting existing requests: %v", url, e)
+		LOG.Warnf("%s: failed to fully load: %v", url, e)
 	} else {
-		page.WaitForLoadState("networkidle")
+		(*page).WaitForLoadState("networkidle")
 	}
 	elapsed := time.Since(start)
+	LOG.Debugf("%s: paged loaded (`networkidle`), took %fs", url, elapsed.Seconds())
+	LOG.Debugf("%s: waiting an additional %fs", url, LOAD_WAIT.Seconds())
+	time.Sleep(LOAD_WAIT)
+	LOG.Debugf("%s: finished waiting", url)
+	LOG.Debugf("%s: got %d requests", url, len(reqs))
 
-	LOG.Infof("Loaded %s, took %fs", url, elapsed.Seconds())
-	LOG.Infof("Got %d requests for %s", len(reqs), url)
-
-	trial := Trial{BrowserVersion: page.Context().Browser().Version(), StartTime: start, Requests: reqs}
+	// Construct trial
+	trial := Trial{BrowserVersion: (*page).Context().Browser().Version(), StartTime: start, Requests: reqs}
 
 	True := true // So we can reference
-	// TODO: Use chan and UpdateMany for efficiency?
-	// TODO: Use different context here?
+	// Insert or update the new trial Debugrmation
 	res, e := collection.UpdateOne(context.Background(), bson.D{{"url", url}, {"category", category}}, bson.D{{"$push", bson.D{{"trials", trial}}}}, &options.UpdateOptions{Upsert: &True})
 	if e != nil {
-		LOG.Errorf("Failed to update trial for %s: %v", url, e)
+		LOG.Errorf("%s: failed to update trial: %v", url, e)
 	}
 	if res.UpsertedID != nil {
-		LOG.Debugf("Upserted for %s", url)
+		LOG.Debugf("%s: created new db entry", url)
 	}
-	LOG.Infof("Inserted new trial for %s", url)
+	LOG.Debugf("%s: inserted new trial", url)
 }
 
-// ! GLOBAL LOGGER
-var LOG *zap.SugaredLogger
+var (
+	// ! GLOBAL LOGGER
+	LOG *zap.SugaredLogger
+)
 
 func main() {
 	//! Init logging
@@ -144,19 +179,7 @@ func main() {
 	defer rawLog.Sync()
 	LOG.Info("Initialized logger")
 
-	//! Load .env
-	e = godotenv.Load()
-	if e != nil {
-		LOG.Warnf("Failed to load .env file, falling back to defaults: %v", e)
-	}
-	LOG.Info("Loaded .env successfully")
-
 	//! Load sites file
-	//Env vars
-	SITES_FILE, found := os.LookupEnv("SITES_FILE")
-	if !found {
-		LOG.Panic("SITES_FILE not found in .env or env")
-	}
 	rawJson, e := os.ReadFile(SITES_FILE)
 	if e != nil {
 		LOG.Panicf("Failed to read sites file at %s: %v", SITES_FILE, e)
@@ -166,90 +189,65 @@ func main() {
 	LOG.Infof("Got site list from %s", SITES_FILE)
 
 	//! Init DB
-	// Env vars
-	MONGO_URI, found := os.LookupEnv("MONGO_URI")
-	if !found {
-		LOG.Warnf("MONGO_URI not found in .env or env, using default of %s", DEFAULT_MONGO_URI)
-		MONGO_URI = DEFAULT_MONGO_URI
-	}
-	DB_NAME, found := os.LookupEnv("DB_NAME")
-	if !found {
-		LOG.Warnf("DB_NAME not found in .env or env, using default of %s", DEFAULT_DB_NAME)
-		DB_NAME = DEFAULT_DB_NAME
-	}
-	COLLECTION_NAME, found := os.LookupEnv("COLLECTION_NAME")
-	if !found {
-		LOG.Warnf("COLLECTION_NAME not found in .env or env, using default of %s", DEFAULT_COLLECTION_NAME)
-		COLLECTION_NAME = DEFAULT_COLLECTION_NAME
-	}
 	// Actual init
 	dbCtx := context.Background()
 	dbClient, e := mongo.Connect(dbCtx, options.Client().ApplyURI(MONGO_URI))
 	if e != nil {
-		LOG.Panic("Failed to connect to DB: ", e)
+		LOG.Panicf("Failed to connect to DB: %v", e)
 	}
 	defer func() {
 		if e := dbClient.Disconnect(dbCtx); e != nil {
-			LOG.Panic("Failed to disconnect from DB: ", e)
+			LOG.Panicf("Failed to disconnect from DB: %v", e)
 		}
 	}()
+
 	// Verify DB connection
 	e = dbClient.Ping(dbCtx, readpref.Primary())
 	if e != nil {
-		LOG.Panic("Client couldn't connect to the DB: ", e)
+		LOG.Panicf("Client couldn't connect to the DB: %v", e)
 	}
 	LOG.Infof("Connected to MongoDB with URI %s", MONGO_URI)
-	// Connect to specific site information collection
+	// Connect to specific site Debugrmation collection
 	collection := dbClient.Database(DB_NAME).Collection(COLLECTION_NAME)
 	LOG.Infof("Connected to `%s` collection on `%s` DB", COLLECTION_NAME, DB_NAME)
 
 	//! Init playwright & browser
-	// Env vars
-	BROWSER_PATH, found := os.LookupEnv("BROWSER_PATH")
-	if !found {
-		LOG.Panic("BROWSER_PATH not found in .env or env")	}
 	// Start Playwright
 	pw, e := playwright.Run()
 	if e != nil {
-		LOG.Panic("Failed to start playwright: ", e)
+		LOG.Panicf("Failed to start playwright: %v", e)
 	}
 	defer pw.Stop()
 	LOG.Info("Started Playwright")
+
 	// Start browser
 	path := BROWSER_PATH
 	browser, e := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{ExecutablePath: &path}) //! Browser customization here
 	if e != nil {
-		LOG.Panic("Failed to launch Chromium instance ", e)
+		LOG.Panicf("Failed to launch browser: %v", e)
 	}
 	defer func() {
 		if e = browser.Close(); e != nil {
-			LOG.Panic("Failed to close browser: ", e)
+			LOG.Panicf("Failed to close browser: %v", e)
 		}
 	}()
 	LOG.Infof("Launched Chromium version %s", browser.Version())
 
 	//! Data collection
-	//Env vars
-	var MAX_TABS int
-	rawMaxTabs, found := os.LookupEnv("MAX_TABS")
-	if found {
-		MAX_TABS, e = strconv.Atoi(rawMaxTabs)
-		if e != nil {
-			LOG.Warnf("MAX_TABS value of %s in .env is not a number, falling back to default of %d", rawMaxTabs, DEFAULT_MAX_TABS)
-		}
-	} else {
-		LOG.Warnf("MAX_TABS not found in .env or env, using default of %s", DEFAULT_MAX_TABS)
-		MAX_TABS = DEFAULT_MAX_TABS
-	}
+	browserChunks := categories.Chunks(MAX_BROWSERS)
+
 	LOG.Info("Starting data collection")
 	var wg sync.WaitGroup
-	for category, sites := range categories {
-		bCtx, e := browser.NewContext() //! Browser customization here
-		if e != nil {
-			LOG.Panicf("Failed to create browser context for %s: %v", category, e)
+	for i, chunk := range browserChunks {
+		LOG.Debugf("Starting browser chunk %d", i)
+		for category, sites := range chunk {
+			bCtx, e := browser.NewContext() //! Browser customization here
+			if e != nil {
+				LOG.Panicf("Failed to create browser context for %s: %v", category, e)
+			}
+			wg.Add(1)
+			go getCategory(&sites, category, &bCtx, collection, &wg)
 		}
-		wg.Add(1)
-		go getCategory(sites, category, bCtx, collection, &wg, MAX_TABS)
+		wg.Wait()
 	}
-	wg.Wait()
 }
