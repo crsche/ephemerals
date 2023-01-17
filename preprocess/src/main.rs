ea9797846ca23e437442c7a93b05629ddbac8588
Conor Scheidt <conorrs@protonmail.com>
2023-01-16 19:31:42 -0600

    Add a preprocessor to determine if sites exist

---
+++ preprocess/src/main.rs
@@ -0,0 +1,148 @@
+#![feature(async_closure)]
+
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+
+use std::{
+	env,
+	fs::{self, File, OpenOptions},
+	io::{BufRead, BufReader, BufWriter, Write},
+};
+
+use anyhow::Result;
+use futures::{stream, StreamExt};
+use hashbrown::HashMap;
+use serde::{Deserialize, Serialize};
+use tokio::task::JoinHandle;
+use trust_dns_resolver::{config::*, error::ResolveErrorKind, TokioAsyncResolver};
+
+const CONF_PATH: &'static str = "../config.toml";
+
+// config.toml, used by both the preprocessor and the collector
+#[derive(Deserialize)]
+struct RawConfig {
+	preprocess: Config,
+}
+
+// Actual config
+#[derive(Deserialize)]
+struct Config {
+	input:               String,
+	exists_out:          String,
+	unexists_out:        String,
+	pretty:              bool,
+	concurrent_requests: usize,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+	env::set_var("RUST_LOG", "info");
+	pretty_env_logger::init();
+	info!("Initialized logging");
+
+	let raw_conf: RawConfig = toml::from_str(&fs::read_to_string(CONF_PATH)?)?;
+	let conf = raw_conf.preprocess;
+	info!("Loaded config from {}", CONF_PATH);
+
+	let in_file = File::open(format!("../{}", conf.input))?;
+	let rdr = BufReader::new(in_file);
+	let input: HashMap<String, Vec<String>> = serde_json::from_reader(rdr)?;
+	info!("Processed input from {}", conf.input);
+
+	// DNS resolver (reused)
+	let resolver =
+		TokioAsyncResolver::tokio(ResolverConfig::cloudflare(), ResolverOpts::default())?;
+
+	// Main "objects" representing existing and unexisting hostnames in the same
+	// form as the input
+	let mut exists = HashMap::with_capacity(input.len());
+	let mut unexists = HashMap::with_capacity(input.len());
+
+	// Iterator over input map
+	let mut it = input.into_iter();
+
+	info!("Beginning data collection");
+	// Split based on hostname existence
+	while let Some((category, hostnames)) = it.next() {
+		// Caches for existing and unexisting hosts
+		let mut existing = Vec::with_capacity(hostnames.len());
+		let mut unexisting = Vec::with_capacity(hostnames.len());
+
+		// Determine if hostnamess exist
+		let mut st = stream::iter(hostnames)
+			.map(|hostname| {
+				let resolver = &resolver;
+				async move {
+					let resp = resolver.lookup_ip(format!("{}.", &hostname)).await;
+					let exists = match resp {
+						Ok(_) => true,
+						Err(e) => {
+							debug!("{}: DNS lookup returned error: {}", &hostname, e);
+							match e.kind() {
+								ResolveErrorKind::NoRecordsFound { .. } => false,
+								_ => true,
+							}
+						}
+					};
+
+					debug!("{} ok: {}", &hostname, exists);
+					(hostname, exists)
+				}
+			})
+			.buffered(conf.concurrent_requests);
+
+		// Insert the hostnames into the local vectors depending on if they exist
+		while let Some((hostname, exists)) = st.next().await {
+			if exists {
+				existing.push(hostname)
+			} else {
+				unexisting.push(hostname)
+			}
+		}
+		info!("{}: pushing vectors", &category);
+		// Add the vectors to the output object
+		exists.insert(category.clone(), existing);
+		unexists.insert(category, unexisting);
+	}
+	info!("Finished data collection");
+
+	// Write output objects to file in parallel
+	let exists_saving: JoinHandle<Result<()>> = tokio::spawn(async move {
+		let mut exists_out = OpenOptions::new()
+			.write(true)
+			.create(true)
+			.open(&conf.exists_out)?;
+		let mut exists_wrtr = BufWriter::new(&mut exists_out);
+		if conf.pretty {
+			serde_json::to_writer_pretty(&mut exists_wrtr, &exists)?;
+		} else {
+			serde_json::to_writer(&mut exists_wrtr, &exists)?;
+		}
+		exists_wrtr.flush()?;
+		info!("Wrote output for existing to {}", conf.exists_out);
+		Ok(())
+	});
+
+	let unexists_saving: JoinHandle<Result<()>> = tokio::spawn(async move {
+		let mut unexists_out = OpenOptions::new()
+			.write(true)
+			.create(true)
+			.open(&conf.unexists_out)?;
+		let mut unexists_wrtr = BufWriter::new(&mut unexists_out);
+		if conf.pretty {
+			serde_json::to_writer_pretty(&mut unexists_wrtr, &unexists)?;
+		} else {
+			serde_json::to_writer(&mut unexists_wrtr, &unexists)?;
+		}
+		unexists_wrtr.flush()?;
+		info!("Wrote output for unexisting to {}", conf.unexists_out);
+		Ok(())
+	});
+
+	// Wait for output to be written
+	exists_saving.await??;
+	unexists_saving.await??;
+
+	Ok(())
+}
